d96a5f23e9b97c091477c7ba8167bc3e030546a7
ParimalMate <153267855+ParimalMate@users.noreply.github.com>
2025-08-16 15:19:00 +0530

    Add files via upload

---
+++ q3.sql
@@ -0,0 +1,9 @@
+DELIMITER //
+CREATE PROCEDURE AddSubscriberIfNotExists(IN subName VARCHAR(100))
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Subscribers WHERE SubscriberName = subName) THEN
+        INSERT INTO Subscribers (SubscriberName, SubscriptionDate)
+        VALUES (subName, CURDATE());
+    END IF;
+END //
+DELIMITER ;
